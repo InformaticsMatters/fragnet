4a3f1d126aafcc39b5b5e048190407aea4c99a02
Alan Christie <alan.christie@matildapeak.com>
2019-01-23 09:52:24 +0000

    - Updated setenv-template

--- orchestration/setenv-template.sh
+++ orchestration/setenv-template.sh
@@ -1,6 +1,6 @@
 # Copy as 'setenv.sh'
 # then 'source setenv.sh' before orchestrating.
 
-export KEYCLOAK_SERVER_URL="https://squonk.it/auth"
-export KEYCLOAK_PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjSZraYYf/0NBuEr8HlbLfoJxerquD0l1Q3ZZP3CiLAU4+qYfR4bph2LsWbxXpWCIaKfLn2bPAy942l6AI3ZOw2dCUBobT1TpPAr7rVYciM9IGsTAyOgac5FYBW7JE4+A4BYJcZCtARRn4BsZ+dZYXQGnEtvCY0LfBIs1BrJqF2oY38xxfytYHP3ZvV1+ngf1mQ/R5XzrXknJGkIB0ZkwmyyCmfn9L96ntO4RXGJrAsx1vybpuCuz5yzuTANbuF2gfQGFEYrshFS7oW6JtNNsUyhwcs7JaCXdKVjpFOcdWgLskc3FajKVKfpekidr0QE16lHUfn9NCVyKAFwEo1fBSwIDAQAB"
-export KEYCLOAK_SECRET="ddd55cb0-c6f5-4e6e-a6ed-c97c26c20d4a"
+export KEYCLOAK_SERVER_URL="SetMe"
+export KEYCLOAK_PUBLIC_KEY="SetMe"
+export KEYCLOAK_SECRET="SetMe"
