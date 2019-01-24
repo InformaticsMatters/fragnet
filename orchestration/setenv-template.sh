d9aea4a9dfe306a412477c4df7739d07fed4960b
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-01-24 17:06:00 +0000

    image based on tomcat 8.5 and keycloak adapters 3.4.3

--- orchestration/setenv-template.sh
+++ orchestration/setenv-template.sh
@@ -1,6 +1,11 @@
 # Copy as 'setenv.sh'
 # then 'source setenv.sh' before orchestrating.
 
+# the location of the Keycloak server. e.g. "https://squonk.it/auth"
 export KEYCLOAK_SERVER_URL="SetMe"
+
+# the public key of the keycloak realm
 export KEYCLOAK_PUBLIC_KEY="SetMe"
+
+# the client secret for fragnet-search client in the realm
 export KEYCLOAK_SECRET="SetMe"
