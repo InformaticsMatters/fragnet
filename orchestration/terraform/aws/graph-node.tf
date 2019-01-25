afe24f59aef124741818c5aef6f0b93714957c39
Alan Christie <alan.christie@matildapeak.com>
2019-01-25 09:11:02 +0000

    - Tidy of terraform files

--- orchestration/terraform/aws/graph-node.tf
+++ orchestration/terraform/aws/graph-node.tf
@@ -29,34 +29,3 @@ resource "aws_eip_association" "graph_node" {
   instance_id   = "${aws_instance.graph-node.id}"
   allocation_id = "${var.aws_graph_eip_id}"
 }
-
-#resource "aws_spot_instance_request" "graph-node" {
-#  ami = "${lookup(var.amis, var.aws_region)}"
-#  instance_type = "r5.2xlarge"
-#  key_name = "${var.aws_key_name}"
-#  vpc_security_group_ids = ["${aws_security_group.neo4j.id}",
-#                            "${aws_security_group.ssh.id}"]
-#  subnet_id = "${module.vpc.public_subnets[0]}"
-#  associate_public_ip_address = true
-#  source_dest_check = false
-#  spot_price = "0.2"
-#  spot_type = "one-time"
-#  wait_for_fulfillment = true
-#
-#  root_block_device {
-#    volume_type = "gp2"
-#    volume_size = "100"
-#  }
-#}
-
-#resource "aws_ebs_volume" "loader" {
-#  availability_zone = "${var.aws_zone}"
-#  type = "gp2"
-#  size = 50
-#}
-
-#resource "aws_volume_attachment" "ebs_att" {
-#  device_name = "/dev/sdh"
-#  volume_id   = "${aws_ebs_volume.loader.id}"
-#  instance_id = "${aws_instance.graph-node.0.id}"
-#}
