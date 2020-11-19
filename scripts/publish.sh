120556c8e4dc70bce4ce2d6ffce27b86c1b328f5
Alan Christie <alan.christie@matildapeak.com>
2020-11-19 13:33:59 +0000

    - Adjust publish for latest image structure (depict and search)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,16 +11,22 @@ echo "FRAGNET_IMAGE_TAG is $FRAGNET_IMAGE_TAG"
 # Login to docker.io
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 
-./gradlew buildDockerImage -x test
-docker push squonk/fragnet-services:"$FRAGNET_IMAGE_TAG"
+./gradlew buildDockerImages -x test
+docker push squonk/fragnet-depict:"$FRAGNET_IMAGE_TAG"
+docker push squonk/fragnet-search:"$FRAGNET_IMAGE_TAG"
 
 # If this is not 'latest'
 # then also tag and push new 'latest' and 'stable' images...
 
 if [[ "$FRAGNET_IMAGE_TAG" != "latest" ]]
 then
-  docker tag squonk/fragnet-services:"$FRAGNET_IMAGE_TAG" squonk/fragnet-services:latest
-  docker push squonk/fragnet-services:latest
-  docker tag squonk/fragnet-services:"$FRAGNET_IMAGE_TAG" squonk/fragnet-services:stable
-  docker push squonk/fragnet-services:stable
+  docker tag squonk/fragnet-depict:"$FRAGNET_IMAGE_TAG" squonk/fragnet-depict:latest
+  docker push squonk/fragnet-depict:latest
+  docker tag squonk/fragnet-depict:"$FRAGNET_IMAGE_TAG" squonk/fragnet-depict:stable
+  docker push squonk/fragnet-depict:stable
+
+  docker tag squonk/fragnet-search:"$FRAGNET_IMAGE_TAG" squonk/fragnet-search:latest
+  docker push squonk/fragnet-search:latest
+  docker tag squonk/fragnet-search:"$FRAGNET_IMAGE_TAG" squonk/fragnet-search:stable
+  docker push squonk/fragnet-search:stable
 fi
